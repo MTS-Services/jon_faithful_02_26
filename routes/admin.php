bccf5a12a19de61d5eedec8f0a8278b6100089ed
Md Monirul Islam Taluqdar <82707883+monirulislam0@users.noreply.github.com>
2026-02-01 14:56:12 +0600

    updated

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminAuthController;
-use App\Http\Controllers\Admin\AdminDashboardController;
-use App\Http\Controllers\Admin\DriverManagementController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->name('admin.')->group(function () {
@@ -11,13 +10,5 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::post('/login', [AdminAuthController::class, 'login'])->name('login.post');
     });
 
-    Route::middleware('admin')->group(function () {
-        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
-        Route::get('/drivers', [DriverManagementController::class, 'index'])->name('drivers.index');
-        Route::post('/drivers', [DriverManagementController::class, 'store'])->name('drivers.store');
-        Route::put('/drivers/{driver}', [DriverManagementController::class, 'update'])->name('drivers.update');
-        Route::post('/drivers/{driver}/approve', [DriverManagementController::class, 'approve'])->name('drivers.approve');
-        Route::delete('/drivers/{driver}', [DriverManagementController::class, 'destroy'])->name('drivers.destroy');
-        Route::post('/logout', [AdminAuthController::class, 'logout'])->name('logout');
-    });
+   
 });
