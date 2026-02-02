af91a439e12356a4cd1da2560e3d8faf5dbf1946
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-02-02 18:16:15 +0600

    some update

--- resources/js/pages/frontend/tennessee-relocation.tsx
+++ resources/js/pages/frontend/tennessee-relocation.tsx
@@ -1,11 +1,16 @@
 import AverageHomePrice from '@/components/sections/frontend/tennessee-relocation/average-home-price'
 import AverageRentalCost from '@/components/sections/frontend/tennessee-relocation/average-rental-cost'
 import ChoosingTheRightArea from '@/components/sections/frontend/tennessee-relocation/choosing-the-right-area'
+import FinalThoughts from '@/components/sections/frontend/tennessee-relocation/final-thoughts'
+import HelpsYou from '@/components/sections/frontend/tennessee-relocation/helps-you'
 import Hero from '@/components/sections/frontend/tennessee-relocation/hero'
 import HomePriceRanges from '@/components/sections/frontend/tennessee-relocation/home-price-ranges'
 import HousingOptions from '@/components/sections/frontend/tennessee-relocation/housing-options'
+import LifestyleCommunity from '@/components/sections/frontend/tennessee-relocation/lifestyle-community'
+import MovingToTennesseeChecklist from '@/components/sections/frontend/tennessee-relocation/moving-to-tennessee-checklist'
 import MovingToTennessee from '@/components/sections/frontend/tennessee-relocation/moving-to-yennessee'
 import Tabs from '@/components/sections/frontend/tennessee-relocation/tabs'
+
 import FrontendLayout from '@/layouts/frontend-layout'
 import React from 'react'
 
@@ -20,6 +25,10 @@ export default function TennesseeRelocation() {
         <AverageHomePrice/>
         <HomePriceRanges/>
         <Tabs/>
+        <LifestyleCommunity/>
+        <MovingToTennesseeChecklist/>
+        <HelpsYou/>
+        <FinalThoughts/>
     </FrontendLayout>
   )
 }
