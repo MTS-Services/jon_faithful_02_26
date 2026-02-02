7549c73c83146da2120e4a680cf54ad3ce028779
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-02-02 15:02:13 +0600

    why tennessee page update

--- resources/js/pages/frontend/why-tennessee.tsx
+++ resources/js/pages/frontend/why-tennessee.tsx
@@ -1,4 +1,9 @@
 import Hero from '@/components/sections/frontend/rentals/hero'
+import Disclaimer from '@/components/sections/frontend/why-tennessee/disclaimer'
+import Guiding from '@/components/sections/frontend/why-tennessee/guiding'
+import OurCommitment from '@/components/sections/frontend/why-tennessee/our-commitment'
+import WhoWeServe from '@/components/sections/frontend/why-tennessee/who-we-serve'
+import WhyTrust from '@/components/sections/frontend/why-tennessee/why-trust'
 import FrontendLayout from '@/layouts/frontend-layout'
 import React from 'react'
 
@@ -6,6 +11,11 @@ export default function WhyTennessee() {
   return (
     <FrontendLayout>
         <Hero/>
+        <Guiding/>
+        <WhoWeServe/>
+        <WhyTrust/>
+        <OurCommitment/>
+        <Disclaimer/>
     </FrontendLayout>
   )
 }
