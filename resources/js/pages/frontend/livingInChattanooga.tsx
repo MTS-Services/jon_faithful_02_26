daaf491e830a7265e752727d0f5664ec55e7d234
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-02-02 09:24:18 +0600

    update

---
+++ resources/js/pages/frontend/livingInChattanooga.tsx
@@ -0,0 +1,11 @@
+
+import Hero from "@/components/sections/frontend/chattanooga/hero"
+import FrontendLayout from "@/layouts/frontend-layout"
+
+export default function LivingInChattanooga() {
+    return (
+        <FrontendLayout>
+            <Hero />
+        </FrontendLayout>
+    )
+}
