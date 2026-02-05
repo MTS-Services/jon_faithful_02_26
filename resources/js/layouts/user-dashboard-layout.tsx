5b61aff08fb7db85b691f738fe9e4cb5904f6367
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-02-05 18:13:37 +0600

    side bar update

--- resources/js/layouts/user-dashboard-layout.tsx
+++ resources/js/layouts/user-dashboard-layout.tsx
@@ -38,12 +38,12 @@ export default function UserDashboardLayout({
                 </div>
 
                 <div className="container mx-auto my-20 bg-gray-50 font-sans">
-                    <div className="flex">
+                    <div className="block md:flex gap-10">
                         {/* LEFT SIDEBAR */}
                         <Sidebar />
 
                         {/* RIGHT CONTENT AREA */}
-                        <div className="flex w-full flex-col px-10">
+                        <div className="flex w-full flex-col">
                             <Header />
 
                             <main className="mt-6">{children}</main>
