0ca45e604f96083e6a934012d9a60f93ff20f406
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-02-17 17:58:28 +0600

    updated

--- resources/js/components/sections/frontend/cost-of-living-in-tennessee/utilities-monthly-bills.tsx
+++ resources/js/components/sections/frontend/cost-of-living-in-tennessee/utilities-monthly-bills.tsx
@@ -40,7 +40,7 @@ export default function UtilitiesMonthlyBills() {
                                     ></path>
                                 </svg>
                                 <span className="text-uted-foreground text-sm">
-                                    Research cities
+                                    Home size and age
                                 </span>
                             </li>
                             <li className="flex items-center gap-2.5">
@@ -56,7 +56,7 @@ export default function UtilitiesMonthlyBills() {
                                     ></path>
                                 </svg>
                                 <span className="text-uted-foreground text-sm">
-                                    Decide rent vs buy
+                                    Apartment vs single-family home
                                 </span>
                             </li>
                             <li className="flex items-center gap-2.5">
@@ -72,7 +72,7 @@ export default function UtilitiesMonthlyBills() {
                                     ></path>
                                 </svg>
                                 <span className="text-uted-foreground text-sm">
-                                    Set a budget
+                                    City or rural location
                                 </span>
                             </li>
                             <li className="flex items-center gap-2.5">
@@ -88,39 +88,7 @@ export default function UtilitiesMonthlyBills() {
                                     ></path>
                                 </svg>
                                 <span className="text-uted-foreground text-sm">
-                                    Arrange utilities
-                                </span>
-                            </li>
-                            <li className="flex items-center gap-2.5">
-                                <svg
-                                    aria-hidden="true"
-                                    className="h-4 w-4 text-black"
-                                    viewBox="0 0 512 512"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fill="currentColor"
-                                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
-                                    ></path>
-                                </svg>
-                                <span className="text-uted-foreground text-sm">
-                                    Update licenses and registrations
-                                </span>
-                            </li>
-                            <li className="flex items-center gap-2.5">
-                                <svg
-                                    aria-hidden="true"
-                                    className="h-4 w-4 text-black"
-                                    viewBox="0 0 512 512"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fill="currentColor"
-                                        d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"
-                                    ></path>
-                                </svg>
-                                <span className="text-uted-foreground text-sm">
-                                    Understand state taxes
+                                    Seasonal energy usage (summer cooling, winter heating)
                                 </span>
                             </li>
                         </ul>
