14443bcb5f61229180a426af406453f63e12bc5e
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2026-04-23 18:30:45 +0600

    Update UserPasswordResetOtpMail.php

--- app/Mail/UserPasswordResetOtpMail.php
+++ app/Mail/UserPasswordResetOtpMail.php
@@ -3,13 +3,12 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class UserPasswordResetOtpMail extends Mailable implements ShouldQueue
+class UserPasswordResetOtpMail extends Mailable
 {
     use Queueable, SerializesModels;
 
